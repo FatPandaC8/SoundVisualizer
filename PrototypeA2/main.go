d8380b3c74ff7da8d2637bbcb96f4726f248ae36
FatPandaC8 <bachcodethings@gmail.com>
2026-02-01 01:16:07 +0700

    FFT for complex, now for samples []int16 mono

--- PrototypeA2/main.go
+++ PrototypeA2/main.go
@@ -1,13 +1,30 @@
 package main
 
 import (
-	"log"
-	"shazam/internal"
+	// "fmt"
+	// "math/cmplx"
+	// "shazam/internal"
+	// "log"
+	// "shazam/internal"
 )
 
+
 func main() {
-	_, err := internal.ReadWav("songs/Uoc_Mua.wav");
-	if err != nil {
-		log.Fatal(err)
-	}
+	// _, err := internal.ReadWav("songs/Uoc_Mua.wav");
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// 8-point square wave: [1,1,1,1,0,0,0,0]
+	// signal := []complex128{1, 1, 1, 1, 0, 0, 0, 0}
+	
+	// spectrum := internal.FFT(signal)
+	
+	// fmt.Println("Frequency Domain:")
+	// for i, val := range spectrum {
+	// 	magnitude := cmplx.Abs(val)
+	// 	phase := cmplx.Phase(val)
+	// 	fmt.Printf("Bin %d: Magnitude=%.2f, Phase=%.2f rad\n", 
+	// 		i, magnitude, phase)
+	// }
 }
+
